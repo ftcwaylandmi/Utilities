c37055f04b701971ebe43bc6abb35dffaacde610
Cat-a-tec-nics <ftc@waylandunion.org>
2020-10-08 18:43:54 -0400

    InitialLoad

--- push.sh
+++ push.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 VERSION=6.0
-CURRHOMEPATH='pwd';
-cd ~/Desktop/FtcRobotController-$VERSION-$1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
-
+CURRHOMEPATH=`pwd`;
+cd FtcRobotController-$VERSION-$1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
 echo "Enter Commit MSG: "
 read commitMSG
 git commit -a -m "$commitMSG"
-
 git push origin master
-
 cd $CURRHOMEPATH;
--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-VERSION=$1
+VERSION=6.0
 curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSION.zip >FtcRobotController-$VERSION.zip
 
 unzip FtcRobotController-$VERSION
 CURRHOMEPATH=`pwd`;
 for i in a b c phones; do
 	cp -r FtcRobotController-$VERSION FtcRobotController-$VERSION-$i
-	cd FtcRobotController-$VERION-$i/TeamCode/src/main/java/org/firstinspires/ftc/
+	cd FtcRobotController-$VERSION-$i/TeamCode/src/main/java/org/firstinspires/ftc/
 	rm -rf teamcode
 	git clone https://github.com/ftcwaylandmi/2020-$i.git teamcode
 	cd $CURRHOMEPATH;
