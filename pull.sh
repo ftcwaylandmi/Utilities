09d13eb6a1f6660439b57f75af2cf0d90fe97c5c
Cat-a-tec-nics <ftc@waylandunion.org>
2020-10-13 18:07:45 -0400

    Push,Pull, and Run Fixes

--- pull.sh
+++ pull.sh
@@ -2,11 +2,13 @@
 
 VERSION=`cat Version.txt`
 CURRHOMEPATH=`pwd`;
-cd Utilities/
 git pull
+echo "Pulled Utilites Repo"
 
 for i in a b c phones; do
+	cd
 	cd ~/FtcRobotController-$VERSION-$i/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
 	git pull
+	echo "Pulled $i Repo"
 	cd $CURRHOMEPATH;
 done
--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 
 VERSION=`cat Version.txt`
 CURRHOMEPATH=`pwd`;
+
 cd FtcRobotController-$VERSION-$1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
 echo "Enter Commit MSG: "
 read commitMSG
