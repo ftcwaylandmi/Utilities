9e6db80d3d2dcf44a2006d0cb14f7afcbb59d538
Cat-a-tec-nics <ftc@waylandunion.org>
2020-10-27 18:38:09 -0400

    Push

--- run.sh
+++ run.sh
@@ -19,9 +19,3 @@ cd
 cd Utilities/
 echo "Opening README.md File"
 start README.md
-cd
-cdC:\\Program\ Files/Android/Android\ Studio/bin/
-start studio64.exe
-echo "Opening Android Studio"
-cd
-cd Utilities/
