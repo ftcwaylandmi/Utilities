cd76295c20a573723df41dbc41e84cf688dc41a1
AlexFirstRobotics <crossalex@waylandunion.org>
2021-09-23 17:04:30 -0400

    fixing push.sh and pull.sh

--- pull.sh
+++ pull.sh
@@ -5,7 +5,6 @@ VERSIONTAG=7.0;
 git pull
 CURRHOMEPATH= `pwd`;
 cd
-cd FtcRobotCode-`date +%Y`
 for i in A B C; do
         cd ~/FtcRobotCode-`date +%Y`/FtcRobotController-$VERSIONTAG-$i/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
         git pull
