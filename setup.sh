cbbe6a56840b83feb32c0bc291da0347ab23b294
AlexFirstRobotics <crossalex@waylandunion.org>
2021-09-23 16:38:11 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,12 +11,14 @@ cd FtcRobotCode-`date +%Y`
 curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSIONTAG.zip >FtcRobotController-$VERSIONTAG.zip
 unzip FtcRobotController-$VERSIONTAG
 
+CURRHOMEPATH=`pwd`;
 for i in A B C; do
 	cp -r FtcRobotController-$VERSIONTAG FtcRobotController-$VERSIONTAG-$i
 	cd FtcRobotController-$VERSIONTAG-$i/TeamCode/src/main/java/org/firstinspires/ftc/
 	rm -rf teamcode
-	git clone https://github.com/ftcwaylandmi/$YEAR-$i.git teamcode
-	cd
+	git clone https://github.com/ftcwaylandmi/`date +%Y`-$i.git teamcode
+	cd $CURRHOMEPATH;
 done
-rm -rf FtcRobotController-$VERSION
-rm -rf FtcRobotController-$VERSION.zip
+
+rm -rf FtcRobotController-$VERSIONTAG
+rm -rf FtcRobotController-$VERSIONTAG.zip
