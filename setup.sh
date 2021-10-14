e8ced771b903423d25b71690dd24de0ebb331671
AlexFirstRobotics <crossalex@waylandunion.org>
2021-10-14 18:21:54 -0400

    Changed how setup.sh runs

--- setup.sh
+++ setup.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-cd
-if [ ! -d FtcRobotCode-`date +%Y` ]
-then
-	rm -rf FtcRobotCode-`date +%Y`
-	mkdir -p FtcRobotCode-`date +%Y`
-fi
-
 echo "Year:"
 read YEAR
 
-echo "Version"
+echo "Version:"
 read VERSION
 
-cd FtcRobotCode-$YEAR
-curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSION.zip >FtcRobotController-$VERSION.zip
-unzip FtcRobotController-$VERSION
+cd /c/Users/MSRobotics12
+if [ ! -d FtcRobotCode-$YEAR-$VERSION ]
+then
+	mkdir -p FtcRobotCode-$YEAR-$VERSION
 
-CURRHOMEPATH=`pwd`;
-for i in A B C D; do
-	cp -r FtcRobotController-$VERSION FtcRobotController-$VERSION-$i
-	cd FtcRobotController-$VERSION-$i/TeamCode/src/main/java/org/firstinspires/ftc/
-	rm -rf teamcode
-	git clone https://github.com/ftcwaylandmi/$YEAR-$i.git teamcode
-	cd $CURRHOMEPATH;
-done
+	cd /c/Users/MSRobotics12/FtcRobotCode-$YEAR-$VERSION
+	curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSION.zip >FtcRobotController-$VERSION.zip
+	unzip FtcRobotController-$VERSION
 
-rm -rf FtcRobotController-$VERSION
-rm -rf FtcRobotController-$VERSION.zip
+	CURRHOMEPATH=`pwd`;
+	for i in A B C D; do
+		cp -r FtcRobotController-$VERSION FtcRobotController-$VERSION-$i
+		cd FtcRobotController-$VERSION-$i/TeamCode/src/main/java/org/firstinspires/ftc/
+		rm -rf teamcode
+		git clone https://github.com/ftcwaylandmi/$YEAR-$i.git teamcode
+		cd $CURRHOMEPATH;
+	done
+
+	rm -rf FtcRobotController-$VERSION
+	rm -rf FtcRobotController-$VERSION.zip
+else
+	echo "Files for Version:$VERSION, and Year:$YEAR have already been created. Attempting to pull down year and version from github"
+	for v in A B C D; do
+		cd /c/Users/MSRobotics12/FtcRobotCode-$YEAR-$VERSION/FtcRobotController-$VERSION-$v/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/
+		git pull
+		cd /c/Users/MSRobotics12/FtcRobotCode-$YEAR-$VERSION
+	done
+fi
